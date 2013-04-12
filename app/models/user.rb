752b343825c936a875fad9fdb760e4f4f79d52bf
Mohamed Magdy <eng.mohamagdy@gmail.com>
2013-04-12 02:35:31 +0200

    Adding some comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ActiveRecord::Base
   attr_accessible :name
 
+  # This method takes the sorting and pagination parameters and select
+  # the top users specified by the "top" parameter and sort the users
+  # with the given "sort" parameters which is a comma separated string. For example level,overall.
+  # params
+  #   sort: sort parameters string comma separated. Default is overall,level
+  #   top: number that specifies top X users. Default is 10
+  #   page: page number. Default is 1
   def self.list(params={})
   	params[:sort] ||= 'overall,level'
   	sort_atts = params[:sort].split(',').select { |att| self.attribute_names.include?(att) }.map { |att| "#{att} DESC" }
